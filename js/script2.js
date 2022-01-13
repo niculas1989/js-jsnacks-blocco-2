a80450db676162ac625acfc40cf39b03c8838fa6
niculas1989 <nicolas.maranzano@libero.it>
2022-01-13 15:35:22 +0100

    add logic befor all (scrip2)

--- js/script2.js
+++ js/script2.js
@@ -3,3 +3,15 @@ console.log('JS OK');
 /*
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
 */
+
+const userNumber = parseInt(prompt('Inserisci un numero'));
+
+
+
+if (userNumber % 2 === 0) {
+    console.log('Il tuo numero è pari');
+    const numberEven = userNumber + 2;
+    console.log(`Il prossimo numero pari è ${numberEven}`);
+} else {
+    alert('Inserisci un altro numero');
+}
