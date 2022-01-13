d92994f3faae37466cf11480b9c7b4434170266e
niculas1989 <nicolas.maranzano@libero.it>
2022-01-13 15:03:55 +0100

    add while for script3

--- js/script2.js
+++ js/script2.js
@@ -1,14 +1,1 @@
 console.log('JS OK');
-
-/*
-Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
-*/
-console.log('JS ok')
-const fakeInvites = [];
-const listOne = ['nicolas', 'matteo', 'vasco'];
-const listTwo = ['maranzano', 'imbimbo', 'rossi'];
-
-for (let k = 0; k < listOne.length && listTwo.length; k++) {
-    let listRandom = listOne[k] + '' + listTwo[k];
-    console.log(listRandom);
-}
--- js/script3.js
+++ js/script3.js
@@ -1,1 +1,25 @@
 console.log('JS OK');
+console.log('Questo è il terzo script.');
+/*
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+*/
+const fakeInvites = [];
+const firstNames = ['nicolas', 'matteo', 'vasco', 'linda', 'adriana'];
+const lastNames = ['maranzano', 'imbimbo', 'rossi', 'bettini', 'arminio'];
+console.log(fakeInvites);
+
+while (fakeInvites.lenght < 3) {
+    const randNum1 = Math.floor(Math.random() * firstNames.lenght);
+    const randNum2 = Math.floor(Math.random() * lastNames.lenght);
+
+    const name = firstNames[randNum1];
+    const lastName = lastNames[randNum2];
+
+    const fakePerson = `${name} ${lastName}`;
+
+    if (!fakeInvites.includes(fakePerson)) {
+        fakeInvites.push(fakePerson);
+    }
+}
+
+console.log(fakeInvites);
