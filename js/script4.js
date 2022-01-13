a91822a9fb09e202907e00eb08bb1a8b43b3357c
niculas1989 <nicolas.maranzano@libero.it>
2022-01-13 16:41:09 +0100

    add solution WHILE - scrip4

--- js/script4.js
+++ js/script4.js
@@ -5,22 +5,46 @@ Crea un array di numeri interi
 e fai la somma di tutti gli elementi che sono in posizione dispari
 */
 
+
+//! CICLO FOR
+// const num = 10;
+// const insieme = [];
+// let sum = 0;
+
+
+// for (let y = 0; y < num; y++) {
+//     let rnd = Math.floor(Math.random() * 101);
+//     insieme.push(rnd);
+//     if (y % 2 != 0) {
+//         sum += rnd;
+//     }
+// }
+
+
+// console.log(insieme);
+// console.log(sum);
+
+
+//! CICLO WHILE
+
 const num = 10;
 const insieme = [];
 let sum = 0;
+let i = 0;
 
-
-for (let y = 0; y < num; y++) {
+while (insieme.length < num) {
     let rnd = Math.floor(Math.random() * 101);
     insieme.push(rnd);
-    if (y % 2 != 0) {
+    i++;
+    if (i % 2 != 0) {
         sum += rnd;
     }
 }
 
-
 console.log(insieme);
 console.log(sum);
+
+
 
 
 
