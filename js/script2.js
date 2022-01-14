6635d579f67c1a33c4d100ec15575bfa476b35c9
niculas1989 <nicolas.maranzano@libero.it>
2022-01-14 09:47:20 +0100

    add changes in script2

--- js/script2.js
+++ js/script2.js
@@ -4,11 +4,26 @@ console.log('JS OK');
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
 */
 
+
+//! VERSIONE normale
+
 let userNumber = parseInt(prompt('Inserisci un numero'));
 
-if (userNumber % 2 != 0) {
+if (userNumber % 2 !== 0) {
     const numberEven = userNumber + 1;
     console.log(numberEven);
 } else {
     console.log(userNumber);
 }
+
+
+//! WHILE
+/*
+let askNumb = parseInt(prompt('Inserisci un numero'));
+
+while (askNumb % 2) {
+    askNumb += 1;
+}
+
+console.log(askNumb);
+*/
